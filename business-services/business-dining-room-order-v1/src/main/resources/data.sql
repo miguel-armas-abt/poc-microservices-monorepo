62084ba82c3000d1a15773f8b0b7aad65d648614
Miguel Armas <miguel.armas.abt@gmail.com>
2023-01-25 11:07:36 -0500

    First commit on config-server

--- business-services/business-dining-room-order-v1/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tables(is_available, table_number, capacity) VALUES (true, 1, 4);
-INSERT INTO tables(is_available, table_number, capacity) VALUES (true, 2, 4);
-INSERT INTO tables(is_available, table_number, capacity) VALUES (false, 3, 4);
-INSERT INTO tables(is_available, table_number, capacity) VALUES (true, 4, 4);
-INSERT INTO tables(is_available, table_number, capacity) VALUES (false, 5, 4);
