fb7b65adfe64d4eb19d0878353960d5a1d7dc5e8
Miguel <miguel.armas.abt@gmail.com>
2024-03-19 19:53:06 -0500

    Fix table-placemente, remove eureka

--- application/backend/business/table-placement-v1/src/main/java/com/demo/bbq/business/TablePlacementApplication.java
+++ application/backend/business/table-placement-v1/src/main/java/com/demo/bbq/business/TablePlacementApplication.java
@@ -2,10 +2,8 @@ package com.demo.bbq.business;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class TablePlacementApplication {
 
 	public static void main(String[] args) {
