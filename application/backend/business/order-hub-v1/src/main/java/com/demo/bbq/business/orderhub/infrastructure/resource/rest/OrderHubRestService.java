5fdf5d1d79bae996789228e21d3e5fed012ba2f5
Miguel <miguel.armas.abt@gmail.com>
2024-03-23 00:45:43 -0500

    Fix environment variables

--- application/backend/business/order-hub-v1/src/main/java/com/demo/bbq/business/orderhub/infrastructure/resource/rest/OrderHubRestService.java
+++ application/backend/business/order-hub-v1/src/main/java/com/demo/bbq/business/orderhub/infrastructure/resource/rest/OrderHubRestService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/bbq/experience/order-hub/v1")
+@RequestMapping("/bbq/bff/order-hub/v1")
 public class OrderHubRestService {
 
 }
