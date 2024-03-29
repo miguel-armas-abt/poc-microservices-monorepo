53c47ba4eeb02aa473e8982df79cb92338fde34c
Miguel <miguel.armas.abt@gmail.com>
2024-03-29 10:50:35 -0500

    Fix pv script

--- devops/k8s/shell-scripts/manifests-builder/builders/persistent-builder.sh
+++ devops/k8s/shell-scripts/manifests-builder/builders/persistent-builder.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
 CHECK_SYMBOL="\033[0;32m\xE2\x9C\x94\033[0m"
+PV_TEMPLATE="persistent-volume.template.yaml"
+PVC_TEMPLATE="persistent-volume-claim.template.yaml"
 
 if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 <APP_NAME> <HOST_MOUNT_PATH> <PERSISTENT_TEMPLATE> <PERSISTENT_TYPE> <MANIFESTS_PATH>"
+    echo "Usage: $0 <APP_NAME> <HOST_MOUNT_PATH> <TEMPLATES_PATH> <PERSISTENT_TYPE> <MANIFESTS_PATH>"
     exit 1
 fi
 
 APP_NAME=$1
 HOST_MOUNT_PATH=$2
-PERSISTENT_TEMPLATE=$3
+TEMPLATES_PATH=$3
 PERSISTENT_TYPE=$4
 MANIFESTS_PATH=$5
 
-template=$(<./templates/$PERSISTENT_TEMPLATE)
-template="${template//APP_NAME/$APP_NAME}"
-
+template=""
 suffix="PV"
 if [ "$PERSISTENT_TYPE" = "PV" ]; then
+  template=$(<"$TEMPLATES_PATH"/$PV_TEMPLATE)
+  template="${template//APP_NAME/$APP_NAME}"
   template="${template//HOST_MOUNT_PATH/$HOST_MOUNT_PATH}"
   suffix=pv
 fi
 if [ "$PERSISTENT_TYPE" = "PVC" ]; then
+  template=$(<"$TEMPLATES_PATH"/$PVC_TEMPLATE)
+  template="${template//APP_NAME/$APP_NAME}"
   suffix=pvc
 fi
 
--- devops/k8s/shell-scripts/manifests-builder/k8s-manifests-builder.sh
+++ devops/k8s/shell-scripts/manifests-builder/k8s-manifests-builder.sh
@@ -2,8 +2,6 @@
 
 #general
 SECRET_TEMPLATE=secret.template.yaml
-PV_TEMPLATE=persistent-volume.template.yaml
-PVC_TEMPLATE=persistent-volume-claim.template.yaml
 CONTAINERS_CSV=./../../../environment/docker/containers-to-run.csv
 MANIFESTS_PATH=./../../manifests
 PARAMETERS_PATH=./../../parameters
@@ -22,4 +20,4 @@ DB_RESOURCES_PATH=$ENVIRONMENT_PATH/databases
 DB_SERVICE_TEMPLATE=db.service.template.yaml
 DB_CONTROLLER_TEMPLATE=db.deployment.template.yaml
 K8S_DB_PARAMETERS_CSV=$PARAMETERS_PATH/k8s-db-manifests.csv
-./k8s-db-manifest-builder.sh $DB_RESOURCES_PATH $SECRET_TEMPLATE $PV_TEMPLATE $PVC_TEMPLATE $TEMPLATES_PATH $DB_SERVICE_TEMPLATE $DB_CONTROLLER_TEMPLATE $K8S_DB_PARAMETERS_CSV $CONTAINERS_CSV $MANIFESTS_PATH
+./k8s-db-manifest-builder.sh $DB_RESOURCES_PATH $SECRET_TEMPLATE $TEMPLATES_PATH $DB_SERVICE_TEMPLATE $DB_CONTROLLER_TEMPLATE $K8S_DB_PARAMETERS_CSV $CONTAINERS_CSV $MANIFESTS_PATH
