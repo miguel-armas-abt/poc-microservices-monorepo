a3f029b5bbbfaab722498202609803c5be22f462
Miguel <miguel.armas.abt@gmail.com>
2024-06-08 00:54:11 -0500

    Change quarkus application file

--- application/backend/business/menu-v2/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-insert into menu_options(id, description, category, product_code) values (1, 'Ají de gallina', 'MAIN', 'MENU0001');
-insert into menu_options(id, description, category, product_code) values (2, 'Chancho al cilindro', 'MAIN', 'MENU0002');
-insert into menu_options(id, description, category, product_code) values (3, 'Jarra de chicha', 'DRINK', 'MENU0003');
-insert into menu_options(id, description, category, product_code) values (4, 'Jarra de maracuyá', 'DRINK', 'MENU0004');
-insert into menu_options(id, description, category, product_code) values (5, 'Torta de chocolate', 'DESSERT', 'MENU0005');
-insert into menu_options(id, description, category, product_code) values (6, 'Pie de manzana', 'DESSERT', 'MENU0006');
