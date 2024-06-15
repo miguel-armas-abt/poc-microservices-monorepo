0e50b65d6791b0b595569f05d3937e632ba19b16
Miguel <miguel.armas.abt@gmail.com>
2024-06-15 07:27:09 -0500

    Update logback-spring.xml and exclude actuator/prometheus in logs

--- application/backend/commons/commons-webflux-v1/src/main/java/com/demo/bbq/commons/tracing/logging/RestServerLogger.java
+++ application/backend/commons/commons-webflux-v1/src/main/java/com/demo/bbq/commons/tracing/logging/RestServerLogger.java
@@ -1,13 +1,13 @@
 package com.demo.bbq.commons.tracing.logging;
 
 import com.demo.bbq.commons.properties.ConfigurationBaseProperties;
+import com.demo.bbq.commons.tracing.logging.constants.LoggingExclusion;
 import com.demo.bbq.commons.tracing.logging.constants.LoggingMessage;
 import com.demo.bbq.commons.tracing.logging.injector.ThreadContextInjectorUtil;
 import com.demo.bbq.commons.tracing.logging.obfuscation.header.HeaderObfuscatorUtil;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -22,13 +22,18 @@ public class RestServerLogger implements WebFilter {
   //ToDo: Add requestBody && responseBody in logs
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-    String method = exchange.getRequest().getMethod().toString();
     String url = exchange.getRequest().getURI().toString();
+
+    if (LoggingExclusion.isExcluded(url)) {
+      return chain.filter(exchange);
+    }
+
+    String method = exchange.getRequest().getMethod().toString();
     Map<String, String> requestHeaders = exchange.getRequest().getHeaders().toSingleValueMap();
     String obfuscatedRequestHeaders = HeaderObfuscatorUtil.process(properties.getObfuscation(), requestHeaders);
 
     ThreadContextInjectorUtil.populateFromHeaders(requestHeaders);
-    ThreadContextInjectorUtil.populateFromRestServerRequest(method, url, obfuscatedRequestHeaders, StringUtils.EMPTY);
+    ThreadContextInjectorUtil.populateFromRestServerRequest(method, url, obfuscatedRequestHeaders, "ToDo");
     log.info(LoggingMessage.REST_SERVER_REQUEST);
 
     return chain.filter(exchange)
@@ -38,8 +43,8 @@ public class RestServerLogger implements WebFilter {
           String httpCode = exchange.getResponse().getStatusCode().toString();
 
           ThreadContextInjectorUtil.populateFromHeaders(responseHeaders);
-          ThreadContextInjectorUtil.populateFromRestServerResponse(method, url, obfuscatedResponseHeaders, StringUtils.EMPTY, httpCode);
+          ThreadContextInjectorUtil.populateFromRestServerResponse(method, url, obfuscatedResponseHeaders, "ToDo", httpCode);
           log.info(LoggingMessage.REST_SERVER_RESPONSE);
         });
-  }
+    }
 }
