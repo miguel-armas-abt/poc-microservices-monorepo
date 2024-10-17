6ffff6d2c781586eeea5be6e4c6401f367bdeaf6
Miguel <miguel.armas.abt@gmail.com>
2024-10-17 15:51:26 -0500

    Update scripts

---
+++ devops/shell-scripts/docker/main.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+source ./../commons.sh
+
+print_title() {
+  echo -e "\n########## ${GREEN} Ejecute una acción con Docker ${NC}##########\n"
+}
+
+script_caller() {
+  $1
+  read -rsn1 -p "Presione la tecla enter para regresar al menú"
+  clear
+  print_title
+}
+
+print_title
+
+options=(
+  "Construir imágenes"
+  "Generar plantilla docker-compose"
+  "Iniciar docker-compose"
+  "Mostrar contenedores"
+  "Recrear docker-compose"
+  "Detener docker-compose"
+  "Eliminar docker-compose"
+  "Salir"
+)
+
+select option in "${options[@]}"; do
+    case $REPLY in
+      1) script_caller "./csv-processor.sh build"; ;;
+      2) script_caller "./compose-file-processor.sh template"; ;;
+      3) script_caller "./compose-file-processor.sh up"; ;;
+      4) script_caller "./compose-file-processor.sh list"; ;;
+      5) script_caller "./compose-file-processor.sh recreate"; ;;
+      6) script_caller "./compose-file-processor.sh stop"; ;;
+      7) script_caller "./compose-file-processor.sh delete"; ;;
+      8) exit; ;;
+      *) echo "Opción inválida" >&2
+    esac
+done
