d55d00dbb4ce5f0495938b0381b130f238181ac9
Miguel <miguel.armas.abt@gmail.com>
2024-10-17 15:46:45 -0500

    Update scripts

--- devops/shell-scripts/docker/compose-file-generator.sh
+++ devops/shell-scripts/docker/compose-file-generator.sh
@@ -6,60 +6,33 @@ COMPONENTS_CSV="./../../../application/components.csv"
 BACKEND_PATH="./../../../application/backend"
 DOCKER_COMPOSE_TEMPLATE=./templates/docker-compose.yml
 SERVICE_TEMPLATE=./templates/service.yml
+DOCKER_COMPOSE_FILE="./../../docker-compose.yml";
 
 build_dependencies() {
   local dependencies=$1
 
-  formatted_dependencies=""
   if [ "$dependencies" != "none" ]; then
-    # divide string by ';'
-    IFS=';' read -r -a parts <<< "$dependencies"
-    first=true
-    for part in "${parts[@]}"; do
-      if $first; then
-        formatted_dependencies+="     - $part\n"
-        first=false
-      else
-        formatted_dependencies+="      - $part\n"
-      fi
-    done
-    formatted_dependencies="depends_on:\n $formatted_dependencies"
+    formatted_dependencies=$(echo "$dependencies" | awk -F';' '{for(i=1;i<=NF;i++) printf "      - %s\n", $i}')
+    echo -e "depends_on:\n$formatted_dependencies"
   fi
-
-  formatted_dependencies=$(echo -e "$formatted_dependencies" | sed '/^$/d') #remove extra empty line
-  echo "$formatted_dependencies"
 }
 
 build_variables() {
   local values_file=$1
   local result=""
 
-  process_section() {
-    local section_name=$1
-    declare -n section_map=$2
-    local key_regex="([a-zA-Z0-9\-]+)"
+  configMaps=$(yq '.configMaps // {}' "$values_file")
+  secrets=$(yq '.secrets // {}' "$values_file")
 
-    if grep -q "$section_name:" "$values_file"; then
-      while IFS=":" read -r key value; do
-        if [[ $key =~ ^[[:space:]]+($key_regex)$ ]]; then
-          key="${BASH_REMATCH[1]}"
-          key=$(echo "$key" | tr '-' '_' | tr '[:lower:]' '[:upper:]') #key
-          value=$(echo "$value" | sed 's/^\s*//;s/^\s*\"//;s/\"\s*$//') #value
-          section_map["$key"]="$value"
-        fi
-      done < <(awk "/$section_name:/{f=1; next} f && /^[[:space:]]/{print} !/^[[:space:]]/{f=0}" "$values_file")
+  for key in $(echo "$configMaps" | yq 'keys | .[]'); do
+    value=$(echo "$configMaps" | yq ".\"$key\"")
+    result+="      - $(echo "$key" | tr '-' '_' | tr '[:lower:]' '[:upper:]')=$value\n"
+  done
 
-      for key in "${!section_map[@]}"; do
-        result+="      - ${key}=${section_map[$key]}\n"
-      done
-    fi
-  }
-
-  declare -A configMaps
-  declare -A secrets
-
-  process_section "configMaps" configMaps
-  process_section "secrets" secrets
+  for key in $(echo "$secrets" | yq 'keys | .[]'); do
+    value=$(echo "$secrets" | yq ".\"$key\"")
+    result+="      - $(echo "$key" | tr '-' '_' | tr '[:lower:]' '[:upper:]')=$value\n"
+  done
 
   echo -e "$result"
 }
@@ -67,23 +40,10 @@ build_variables() {
 build_volumes() {
   local volumes=$1
 
-  formatted_volumes=""
   if [ "$volumes" != "none" ]; then
-    # divide string by ';'
-    IFS=';' read -r -a parts <<< "$volumes"
-    first=true
-    for part in "${parts[@]}"; do
-      if $first; then
-        formatted_volumes+="     - $part\n"
-        first=false
-      else
-        formatted_volumes+="      - $part\n"
-      fi
-    done
-    formatted_volumes="volumes:\n $formatted_volumes"
+    formatted_volumes=$(echo "$volumes" | awk -F';' '{for(i=1;i<=NF;i++) printf "      - %s\n", $i}')
+    echo -e "volumes:\n$formatted_volumes"
   fi
-  formatted_volumes=$(echo -e "$formatted_volumes" | sed '/^$/d') #remove extra empty line
-  echo "$formatted_volumes"
 }
 
 process_csv_record() {
@@ -94,15 +54,16 @@ process_csv_record() {
   component_path="$BACKEND_PATH/$component_type/$component_name"
   values_file="$component_path/values.yaml"
 
-  repository=$(awk '/repository:/ {print $2}' "$values_file" | sed 's/"//g')
-  tag_version=$(awk '/tag:/ {print $2}' "$values_file" | sed 's/"//g')
+  repository=$(yq '.image.repository' "$values_file")
+  tag_version=$(yq '.image.tag' "$values_file")
   docker_image="$repository:$tag_version"
-  host_port=$(awk '/hostPort:/ {print $2}' "$values_file" | sed 's/"//g')
-  container_port=$(awk '/containerPort:/ {print $2}' "$values_file" | sed 's/"//g')
-  dependencies=$(awk '/dependencies:/ {print $2}' "$values_file" | sed 's/"//g')
-  volumes=$(awk '/volumes:/ {print $2}' "$values_file" | sed 's/"//g')
+  host_port=$(yq '.docker.hostPort' "$values_file")
+  container_port=$(yq '.ports.containerPort' "$values_file")
+  dependencies=$(yq '.docker.dependencies // "none"' "$values_file")
+  volumes=$(yq '.docker.volumes // "none"' "$values_file")
 
   formatted_service=$(<"$SERVICE_TEMPLATE")
+  formatted_service="${formatted_service//@component_path/$component_path}"
   formatted_service="${formatted_service//@app_name/$component_name}"
   formatted_service="${formatted_service//@docker_image/$docker_image}"
   formatted_service="${formatted_service//@host_port/$host_port}"
@@ -111,9 +72,9 @@ process_csv_record() {
   dependencies=$(build_dependencies "$dependencies")
   formatted_service="${formatted_service//@dependencies/$dependencies}"
 
-  if grep -q 'secrets:' "$values_file" || grep -q 'configMaps:' "$values_file"; then
-      variables=$(build_variables "$values_file")
-      formatted_service="${formatted_service//@variables/"environment: \n$variables"}"
+  variables=$(build_variables "$values_file")
+  if [ -n "$variables" ]; then
+    formatted_service="${formatted_service//@variables/"environment:\n$variables"}"
   else
     formatted_service="${formatted_service//@variables/""}"
   fi
@@ -137,7 +98,7 @@ iterate_csv_records() {
     fi
 
     # Ignore comments
-    if [[ $app_name != "#"* ]]; then
+    if [[ $component_name != "#"* ]]; then
       template_accumulator=$(process_csv_record "$template_accumulator" "$component_name" "$component_type")
     fi
 
@@ -145,10 +106,14 @@ iterate_csv_records() {
   echo "$template_accumulator"
 }
 
-docker_compose_template=$(<"$DOCKER_COMPOSE_TEMPLATE")
-services=$(iterate_csv_records)
-docker_compose_template="${docker_compose_template//@services/$services}"
+build_compose_file() {
+  docker_compose_template=$(<"$DOCKER_COMPOSE_TEMPLATE")
+  services=$(iterate_csv_records)
+  docker_compose_template="${docker_compose_template//@services/$services}"
+  docker_compose_template=$(echo "$docker_compose_template" | sed '/^[[:space:]]*$/d') #delete empty lines
 
-output_file="./../../docker-compose.yml"
-echo -e "$docker_compose_template" > "$output_file"
-echo -e "${CHECK_SYMBOL} created: $output_file"
+  echo -e "$docker_compose_template" > "$DOCKER_COMPOSE_FILE"
+  echo -e "${CHECK_SYMBOL} created: $DOCKER_COMPOSE_FILE"
+}
+
+build_compose_file
