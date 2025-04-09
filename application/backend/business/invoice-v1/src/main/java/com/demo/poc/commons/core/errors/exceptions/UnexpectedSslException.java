15c41a137181b81b624d3e527eabddbc51d60793
Miguel Armas <marmasa@proveedor.intercorp.com.pe>
2025-04-08 19:13:07 -0500

    Update invoice exceptions

--- application/backend/business/invoice-v1/src/main/java/com/demo/poc/commons/core/errors/exceptions/UnexpectedSslException.java
+++ application/backend/business/invoice-v1/src/main/java/com/demo/poc/commons/core/errors/exceptions/UnexpectedSslException.java
@@ -1,6 +1,5 @@
 package com.demo.poc.commons.core.errors.exceptions;
 
-import com.demo.poc.commons.core.errors.dto.ErrorDTO;
 import com.demo.poc.commons.core.errors.enums.ErrorDictionary;
 import lombok.Getter;
 
@@ -8,14 +7,6 @@ import lombok.Getter;
 public class UnexpectedSslException extends GenericException {
 
   public UnexpectedSslException(String message) {
-    super(message);
-
-    ErrorDictionary detail = ErrorDictionary.parse(this.getClass());
-    this.httpStatus = detail.getHttpStatus();
-    this.errorDetail = ErrorDTO.builder()
-            .type(detail.getType())
-            .code(detail.getCode())
-            .message(detail.getMessage())
-            .build();
+    super(message, ErrorDictionary.parse(UnexpectedSslException.class));
   }
 }
--- application/backend/business/invoice-v1/src/main/java/com/demo/poc/commons/custom/exceptions/PaymentStatusNonUpdatedException.java
+++ application/backend/business/invoice-v1/src/main/java/com/demo/poc/commons/custom/exceptions/PaymentStatusNonUpdatedException.java
@@ -1,6 +1,5 @@
 package com.demo.poc.commons.custom.exceptions;
 
-import com.demo.poc.commons.core.errors.dto.ErrorDTO;
 import com.demo.poc.commons.core.errors.enums.ErrorDictionary;
 import com.demo.poc.commons.core.errors.exceptions.GenericException;
 import lombok.Getter;
@@ -9,14 +8,6 @@ import lombok.Getter;
 public class PaymentStatusNonUpdatedException extends GenericException {
 
   public PaymentStatusNonUpdatedException() {
-    super(ErrorDictionary.PAYMENT_STATUS_NON_UPDATED_EXCEPTION.getMessage());
-
-    ErrorDictionary detail = ErrorDictionary.parse(this.getClass());
-    this.httpStatus = detail.getHttpStatus();
-    this.errorDetail = ErrorDTO.builder()
-            .type(detail.getType())
-            .code(detail.getCode())
-            .message(detail.getMessage())
-            .build();
+    super(ErrorDictionary.PAYMENT_STATUS_NON_UPDATED_EXCEPTION.getMessage(), ErrorDictionary.parse(PaymentStatusNonUpdatedException.class));
   }
 }
