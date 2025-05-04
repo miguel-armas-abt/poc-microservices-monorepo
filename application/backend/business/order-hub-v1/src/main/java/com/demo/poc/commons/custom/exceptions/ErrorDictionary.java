16256f5804271e8f7ff1ab4d952ad99c9c9565ec
Miguel <miguel.armas.abt@gmail.com>
2025-05-04 18:01:14 -0500

    Upgrade Core Library - order-hub-v1

--- application/backend/business/order-hub-v1/src/main/java/com/demo/poc/commons/custom/exceptions/ErrorDictionary.java
+++ application/backend/business/order-hub-v1/src/main/java/com/demo/poc/commons/custom/exceptions/ErrorDictionary.java
@@ -1,6 +1,5 @@
 package com.demo.poc.commons.custom.exceptions;
 
-import com.demo.poc.commons.core.errors.dto.ErrorType;
 import com.demo.poc.commons.core.errors.exceptions.GenericException;
 import com.demo.poc.commons.core.errors.exceptions.InvalidFieldException;
 import com.demo.poc.commons.core.errors.exceptions.InvalidStreamingData;
@@ -11,12 +10,10 @@ import com.demo.poc.commons.core.errors.exceptions.NoSuchRestClientErrorExtracto
 import com.demo.poc.commons.core.errors.exceptions.UnexpectedSslException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 import java.util.Arrays;
 
-import static com.demo.poc.commons.core.errors.dto.ErrorType.BUSINESS;
-import static com.demo.poc.commons.core.errors.dto.ErrorType.SYSTEM;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
@@ -26,24 +23,23 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 public enum ErrorDictionary {
 
   //system: 10.00.xx
-  UNEXPECTED_SSL_EXCEPTION("10.00.01", "Unexpected SSL error for HTTP client", SYSTEM, INTERNAL_SERVER_ERROR, UnexpectedSslException.class),
-  INVALID_STREAMING_DATA("10.00.02", "Streaming data is not processable", SYSTEM, INTERNAL_SERVER_ERROR, InvalidStreamingData.class),
-  ERROR_READING_JSON("10.00.03", "Error reading JSON", SYSTEM, INTERNAL_SERVER_ERROR, JsonReadException.class),
+  UNEXPECTED_SSL_EXCEPTION("10.00.01", "Unexpected SSL error for HTTP client", INTERNAL_SERVER_ERROR, UnexpectedSslException.class),
+  INVALID_STREAMING_DATA("10.00.02", "Streaming data is not processable", INTERNAL_SERVER_ERROR, InvalidStreamingData.class),
+  ERROR_READING_JSON("10.00.03", "Error reading JSON", INTERNAL_SERVER_ERROR, JsonReadException.class),
 
   //no such properties and components: 10.01.xx
-  NO_SUCH_REST_CLIENT("10.01.01", "No such rest client", SYSTEM, INTERNAL_SERVER_ERROR, NoSuchRestClientException.class),
-  NO_SUCH_REST_CLIENT_ERROR_EXTRACTOR("10.01.02", "No such rest client error extractor", SYSTEM, INTERNAL_SERVER_ERROR, NoSuchRestClientErrorExtractorException.class),
-  NO_SUCH_PARAM_MAPPER("10.01.03", "No such param mapper", SYSTEM, INTERNAL_SERVER_ERROR, NoSuchParamMapperException.class),
-  NO_SUCH_MENU_REPOSITORY_STRATEGY("10.01.04", "No such menu repository strategy", SYSTEM, INTERNAL_SERVER_ERROR, NoSuchMenuRepositoryStrategyException.class),
+  NO_SUCH_REST_CLIENT("10.01.01", "No such rest client", INTERNAL_SERVER_ERROR, NoSuchRestClientException.class),
+  NO_SUCH_REST_CLIENT_ERROR_EXTRACTOR("10.01.02", "No such rest client error extractor", INTERNAL_SERVER_ERROR, NoSuchRestClientErrorExtractorException.class),
+  NO_SUCH_PARAM_MAPPER("10.01.03", "No such param mapper", INTERNAL_SERVER_ERROR, NoSuchParamMapperException.class),
+  NO_SUCH_MENU_REPOSITORY_STRATEGY("10.01.04", "No such menu repository strategy", INTERNAL_SERVER_ERROR, NoSuchMenuRepositoryStrategyException.class),
 
   //business and bad requests: 10.02.xx
-  INVALID_FIELD("10.02.01", "Invalid field", BUSINESS, BAD_REQUEST, InvalidFieldException.class),
-  MENU_OPTION_NOT_FOUND("10.02.02", "The menu option does not exist", BUSINESS, NOT_FOUND, MenuOptionNotFoundException.class),;
+  INVALID_FIELD("10.02.01", "Invalid field", BAD_REQUEST, InvalidFieldException.class),
+  MENU_OPTION_NOT_FOUND("10.02.02", "The menu option does not exist", NOT_FOUND, MenuOptionNotFoundException.class),;
 
   private final String code;
   private final String message;
-  private final ErrorType type;
-  private final HttpStatus httpStatus;
+  private final HttpStatusCode httpStatus;
   private final Class<? extends GenericException> exceptionClass;
 
   public static ErrorDictionary parse(Class<? extends GenericException> exceptionClass) {
