cf8f9787fe7077a83a2d890264746c8f89881e21
Miguel <miguel.armas.abt@gmail.com>
2025-05-04 18:03:23 -0500

    Upgrade Core Library - table-placement-v1

--- application/backend/business/table-placement-v1/src/main/java/com/demo/poc/commons/custom/exceptions/ErrorDictionary.java
+++ application/backend/business/table-placement-v1/src/main/java/com/demo/poc/commons/custom/exceptions/ErrorDictionary.java
@@ -1,6 +1,5 @@
 package com.demo.poc.commons.custom.exceptions;
 
-import com.demo.poc.commons.core.errors.dto.ErrorType;
 import com.demo.poc.commons.core.errors.exceptions.GenericException;
 import com.demo.poc.commons.core.errors.exceptions.InvalidFieldException;
 import com.demo.poc.commons.core.errors.exceptions.InvalidStreamingData;
@@ -11,12 +10,10 @@ import com.demo.poc.commons.core.errors.exceptions.NoSuchRestClientException;
 import com.demo.poc.commons.core.errors.exceptions.UnexpectedSslException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 import java.util.Arrays;
 
-import static com.demo.poc.commons.core.errors.dto.ErrorType.BUSINESS;
-import static com.demo.poc.commons.core.errors.dto.ErrorType.SYSTEM;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
@@ -26,23 +23,22 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 public enum ErrorDictionary {
 
   //system: 07.00.xx
-  UNEXPECTED_SSL_EXCEPTION("07.00.01", "Unexpected SSL error for HTTP client", SYSTEM, INTERNAL_SERVER_ERROR, UnexpectedSslException.class),
-  INVALID_STREAMING_DATA("07.00.02", "Streaming data is not processable", SYSTEM, INTERNAL_SERVER_ERROR, InvalidStreamingData.class),
-  ERROR_READING_JSON("07.00.03", "Error reading JSON", SYSTEM, INTERNAL_SERVER_ERROR, JsonReadException.class),
+  UNEXPECTED_SSL_EXCEPTION("07.00.01", "Unexpected SSL error for HTTP client", INTERNAL_SERVER_ERROR, UnexpectedSslException.class),
+  INVALID_STREAMING_DATA("07.00.02", "Streaming data is not processable", INTERNAL_SERVER_ERROR, InvalidStreamingData.class),
+  ERROR_READING_JSON("07.00.03", "Error reading JSON", INTERNAL_SERVER_ERROR, JsonReadException.class),
 
   //no such properties and components: 07.01.xx
-  NO_SUCH_REST_CLIENT("07.01.01", "No such rest client", SYSTEM, INTERNAL_SERVER_ERROR, NoSuchRestClientException.class),
-  NO_SUCH_REST_CLIENT_ERROR_EXTRACTOR("07.01.02", "No such rest client error extractor", SYSTEM, INTERNAL_SERVER_ERROR, NoSuchRestClientErrorExtractorException.class),
-  NO_SUCH_PARAM_MAPPER("07.01.03", "No such param mapper", SYSTEM, BAD_REQUEST, NoSuchParamMapperException.class),
+  NO_SUCH_REST_CLIENT("07.01.01", "No such rest client", INTERNAL_SERVER_ERROR, NoSuchRestClientException.class),
+  NO_SUCH_REST_CLIENT_ERROR_EXTRACTOR("07.01.02", "No such rest client error extractor", INTERNAL_SERVER_ERROR, NoSuchRestClientErrorExtractorException.class),
+  NO_SUCH_PARAM_MAPPER("07.01.03", "No such param mapper", INTERNAL_SERVER_ERROR, NoSuchParamMapperException.class),
 
   //business and bad requests: 07.02.xx
-  INVALID_FIELD("07.02.01", "Invalid field", BUSINESS, BAD_REQUEST, InvalidFieldException.class),
-  TABLE_NOT_FOUND("07.02.01", "The table does not exist", BUSINESS, NOT_FOUND, TableNotFoundException.class);
+  INVALID_FIELD("07.02.01", "Invalid field", BAD_REQUEST, InvalidFieldException.class),
+  TABLE_NOT_FOUND("07.02.01", "The table does not exist", NOT_FOUND, TableNotFoundException.class);
 
   private final String code;
   private final String message;
-  private final ErrorType type;
-  private final HttpStatus httpStatus;
+  private final HttpStatusCode httpStatus;
   private final Class<? extends GenericException> exceptionClass;
 
   public static ErrorDictionary parse(Class<? extends GenericException> exceptionClass) {
