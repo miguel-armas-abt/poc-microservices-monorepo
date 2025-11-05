8b80202755f178975a16160102f5cedfa165755e
Miguel <miguel.armas.abt@gmail.com>
2025-11-04 19:10:40 -0500

    refactor : refactor package of config-server-v1

--- backend/microservices/config-server-v1/src/main/java/com/demo/service/ConfigServerApplication.java
+++ backend/microservices/config-server-v1/src/main/java/com/demo/service/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.demo.poc.infrastructure.configserver;
+package com.demo.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
